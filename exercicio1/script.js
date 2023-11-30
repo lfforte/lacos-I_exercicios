83bab619b9760741d12e2842638d0d5d4ef392b3
LuizFernandoForte <lfforte@gmail.com>
2023-11-30 08:47:14 -0300

    Solução exercício 1 de laços de repetição

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+let conta = 0, addPedido = "S";
+
+while(addPedido === "s" || addPedido === "S"){
+    
+    addPedido = prompt(`GostariaPedir outra coxinha? (S)Sim ou (N)Não`);
+    conta +=5;
+
+}
+console.log(`Sua conta foi de ${conta}`);
